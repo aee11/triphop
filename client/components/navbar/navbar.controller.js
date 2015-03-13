695ec41cf056e3c9bf7e0fc998fa609eed11dcec
Alexander Elis <alex2789@gmail.com>
2015-03-13 18:54:05 +0000

    First commit

---
+++ client/components/navbar/navbar.controller.js
@@ -0,0 +1,15 @@
+'use strict';
+
+angular.module('triphopApp')
+  .controller('NavbarCtrl', function ($scope, $location) {
+    $scope.menu = [{
+      'title': 'Home',
+      'link': '/'
+    }];
+
+    $scope.isCollapsed = true;
+
+    $scope.isActive = function(route) {
+      return route === $location.path();
+    };
+  });
